914a5d2e1a5097664e53d5ebf6b79e31c3e2539b
Yuki Honda <dahlia.15s2@gmail.com>
2023-05-28 22:26:11 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//const itemWrapper = document.querySelector('.side-scroll__itemOuter');
+//const itemInner = document.querySelector('.side-scroll__itemInner');
+//
+//gsap.to(itemInner, {
+//  x: () => -(itemInner.clientWidth - itemWrapper.clientWidth),
+//  ease: 'none',
+//  scrollTrigger: {
+//    trigger: '.side-scroll__section', 
+//    start: 'top top',
+//    end: () => `+=${itemInner.clientWidth - itemWrapper.clientWidth}`,
+//    markers: true,
+//    scrub: true, 
+//    pin: true,  
+//    invalidateOnRefresh: true, 
+//    anticipatePin: 1, 
+//  },
+//});
+
